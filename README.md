2a03e8df3f935250eb4faad8a98c729d9ea682d2
Daniel Castro <dan1elcastro@proton.me>
2022-12-02 17:13:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FEM - Time Tracking Dashboard
+
+- ### [Live preview 🎨](https://rwxdan.github.io/time-tracking-dashboard/)
+- ### Desktop product 💻
+  ![](./assets/screenshots/desktop_product.png)
+- ### Mobile product 📱
+  ![](./assets/screenshots/mobile_product.png)
---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+const profile = document.querySelector(".report__profile");
+const actWork = document.querySelector(".report__activity--work");
+const actPlay = document.querySelector(".report__activity--play");
+const actStudy = document.querySelector(".report__activity--study");
+const actExercise = document.querySelector(".report__activity--exercise");
+const actSocial = document.querySelector(".report__activity--social");
+const actSelfCare = document.querySelector(".report__activity--self-care");
+
+const activitiesArr = [
+  actWork,
+  actPlay,
+  actStudy,
+  actExercise,
+  actSocial,
+  actSelfCare,
+];
+
+const reproduceMotion = () => {
+  profile.classList.add("motion");
+  for (let i = 0; i < activitiesArr.length; i++) {
+    activitiesArr[i].classList.add("motion");
+    activitiesArr[0].addEventListener("animationend", () => {
+      setTimeout(() => {
+        activitiesArr[i].classList.add("post");
+      }, 400);
+    });
+  }
+};
+
+window.onload = reproduceMotion();
